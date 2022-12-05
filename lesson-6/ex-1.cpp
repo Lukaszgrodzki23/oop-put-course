2bed624c859b4a96131d99ccc45681971106f47b
Lukaszgrodzki23 <115477342+Lukaszgrodzki23@users.noreply.github.com>
2022-12-05 14:25:00 +0100

    Create ex-1.cpp

---
+++ lesson-6/ex-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Operations {
+private:
+    class Solution
+    {
+        int x;
+        Solution(x)
+        {
+            this->x = x;
+        };
+        int answer()
+        {
+            return this.x
+        };
+    };
+public:
+    max(int a, int b) { return a > b ? Solution(a) : Solution(b) ;}
+    min(int a, int b) { return a < b ? Solution(a) : Solution(b);}
+    avg(int a, int b) { return Solution((a + b) / 2);}
+};
+
+int main()
+{
+    
+}
